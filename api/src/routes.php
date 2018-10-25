444b300cc0b69a5ce937db8ee70a0c6616dee79f
dmcblue <dmcblue@gmail.com>
2018-10-25 18:00:09 -0400

    Convert haxe classes

--- api/src/routes.php
+++ api/src/routes.php
@@ -22,6 +22,25 @@ typedef RequestHandler =
 }
 //*/
 
+function convertHaxeValues($value) {
+	$converted = $value;
+	if(!is_bool($value) && !is_numeric($value) && !is_string($value) && $value !== null) {
+		switch(get_class($value)) {
+			case '_hx_array' :
+				$converted = array_map('convertHaxeValues', $value->a);
+				break;
+			case '_hx_anonymous' :
+				//$converted = array_map('convertHaxeValues', $value->a);
+				$converted = [];
+				foreach($value as $k => $v) {
+					$converted[$k] = convertHaxeValues($v);
+				}
+				break;
+		}
+	}
+	return $converted;
+}
+
 $requestHandlers = require($environmentPath.getenv('REQUEST_HANDLERS_PATH'));
 foreach($requestHandlers as $requestHandler) {
 	$method = strtolower($requestHandler->type->tag);
@@ -30,7 +49,10 @@ foreach($requestHandlers as $requestHandler) {
 		$requestHandler->path, 
 		function (Request $request, Response $response, array $args) use ($handler) {
 			$serverRequest = new \InterealmGames\Server\Http\Slim\Request($request);
-			return $response->withJson($handler($serverRequest));
+			$value = $handler($serverRequest);
+			
+			$output = convertHaxeValues($value);
+			return $response->withJson($output);
 		}
 	);
 }
